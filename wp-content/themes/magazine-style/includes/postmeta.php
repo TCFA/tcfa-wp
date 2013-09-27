6cc09d67530aa4ff340f2e15824f1dd1d25e52cb
Snurppa <snurppa@gmail.com>
2013-09-27 21:50:57 +0300

    comment out ltposts.php

--- wp-content/themes/magazine-style/includes/postmeta.php
+++ wp-content/themes/magazine-style/includes/postmeta.php
@@ -4,7 +4,7 @@
 		<ul class="auth"> <?php magazine_post_meta_data(); ?>, in <?php the_category(', '); ?>
 </ul>
 <ul class="comp">
-		<?php if ( comments_open() ) : ?><?php comments_popup_link('No Comment', '1 Comment', '% Comments'); ?><?php endif; ?> 
+		<?php if ( comments_open() ) : ?><?php comments_popup_link(__('No Comment', 'magazine'), __('1 Comment', 'magazine'), __('% Comments', 'magazine')); ?><?php endif; ?> 
 			</ul>
 	</div> <!-- end post_meta -->
 </span>
