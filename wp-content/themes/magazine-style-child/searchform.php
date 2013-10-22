54f72799b34da52cceca76c8b17aead1b8b9e4d0
Snurppa <snurppa@gmail.com>
2013-10-22 22:34:26 +0300

    translations

--- wp-content/themes/magazine-style-child/searchform.php
+++ wp-content/themes/magazine-style-child/searchform.php
@@ -1,6 +1,6 @@
 <form method="get" id="searchform" action="<?php echo esc_url(home_url()); ?>/">
 	<div>
 	<input type="text" value="<?php _e('Search', 'magazine-child') ?>" name="s" id="s" onfocus="if (this.value == '<?php _e('Search', 'magazine-child') ?>') {this.value = '';}" onblur="if (this.value == '') {this.value = '<?php _e('Search', 'magazine-child') ?>';}" />
-		<input type="submit" id="searchsubmit" value="Go" />
+		<input type="submit" id="searchsubmit" value="<?php _e('Go','magazine-child') ?>" />
 	</div>
 </form>
