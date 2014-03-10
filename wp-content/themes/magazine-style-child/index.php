9d6c1e8d7ec6735f06b8977961eb5007a73fed20
Snurppa <snurppa@gmail.com>
2014-03-10 12:42:37 +0100

    betway always on top

--- wp-content/themes/magazine-style-child/index.php
+++ wp-content/themes/magazine-style-child/index.php
@@ -5,14 +5,20 @@
 			<?php if (magazine_get_option('magazine_banner' ) =='1' ) {load_template(get_template_directory() . '/includes/banner-top.php'); } ?>
 			
 				<div id="content"><div id="subtitle"><?php _e('Frontpage', 'magazine'); // magazine_breadcrumbs(); ?></div>
+					<?php // Betway
+					$q = new WP_Query('tag=betway');
+					while ( $q->have_posts() ) {
+						$q->the_post();
+						get_template_part('/includes/betway'); 
+					}
+					wp_reset_postdata();
+					?>
 					<?php if(have_posts()) : ?>
 					<?php while(have_posts())  : the_post(); ?>
 							<?php
 							// Check if betway post
-							if ( has_tag('betway') ) {
-								get_template_part('/includes/betway'); 
-							} else {
-								get_template_part('/includes/post'); 
+							if ( !has_tag('betway') ) {
+								get_template_part('/includes/post');
 							}
 							?>
 					<?php endwhile; ?>
